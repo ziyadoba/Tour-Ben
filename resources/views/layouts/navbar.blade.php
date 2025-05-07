a6c5a9bdc0f96236541983fe453d4c69a0094938
Odjo Ziyad <odjodiclane@gmail.com>
2025-05-07 22:50:25 +0200

    fist commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="" class="navbar-brand p-0">
+                    <h1 class="m-0"><i class="fa fa-map-marker-alt me-3"></i>Travela</h1>
+                    <!-- <img src="img/logo.png" alt="Logo"> -->
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="index.html" class="nav-item nav-link active">Home</a>
+                        <a href="{{route('about')}}"  class="nav-item nav-link">About</a>
+                        <a href="{{route('services')}}" class="nav-item nav-link">Services</a>
+                        <a href="{{route('packages')}}" class="nav-item nav-link">Packages</a>
+                        <a href="blog.html" class="nav-item nav-link">Blog</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu m-0">
+                                <a href="destination.html" class="dropdown-item">Destination</a>
+                                <a href="tour.html" class="dropdown-item">Explore Tour</a>
+                                <a href="booking.html" class="dropdown-item">Travel Booking</a>
+                                <a href="gallery.html" class="dropdown-item">Our Gallery</a>
+                                <a href="guides.html" class="dropdown-item">Travel Guides</a>
+                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                                <a href="404.html" class="dropdown-item">404 Page</a>
+                            </div>
+                        </div>
+                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                    </div>
+                    <a href="" class="btn btn-primary rounded-pill py-2 px-4 ms-lg-4">Book Now</a>
+                </div>
+            </nav>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('index');});
+Route::get('about', function () {return view('about');})->name('about');
+Route::get('blog', function () {return view('blog');})->name('blog');
+Route::get('booking', function () {return view('booking');})->name('booking');
+Route::get('contact', function () {return view('contact');})->name('contact');
+Route::get('destination', function () {return view('destination');})->name('destination');
+Route::get('gallery', function () {return view('gallery');})->name('gallery');
+Route::get('packages', function () {return view('packages');})->name('packages');
