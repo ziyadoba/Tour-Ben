aa4134ccd72b496598a40404dbcc62fd04dc7824
ziyadoba <odjodiclane@gmail.com>
2025-05-14 16:47:54 +0100

    Ajout de la structure HTML et des feuilles de style pour le navbar et le topbar

--- resources/views/client/layouts/topbar.blade.php
+++ resources/views/client/layouts/topbar.blade.php
@@ -1,3 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
 <div class="container-fluid bg-primary px-5 d-none d-lg-block">
     <div class="row gx-0">
         <div class="col-lg-4 ms-auto text-end">
@@ -73,7 +104,8 @@
                 <form id="registerForm">
                     <div class="row g-3">
                         <div class="col-md-6 form-floating">
-                            <input type="text" class="form-control" id="nom" placeholder="Votre nom" required>
+                            <input type="text" class="form-control" id="nom" placeholder="Votre nom"
+                                required>
                             <label for="nom">Nom</label>
                         </div>
                         <div class="col-md-6 form-floating">
@@ -116,3 +148,16 @@
 
 <!-- Inclure Bootstrap JS (via CDN ou local) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+<!-- JavaScript Libraries -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="lib/lightbox/js/lightbox.min.js"></script>
+
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
